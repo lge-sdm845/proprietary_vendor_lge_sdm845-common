9c6a4d99a935328d5c6b250687c682ac719e4479
Tuan Anh <tuan73176@gmail.com>
2024-04-18 22:48:13 +0700

    sdm845-common: Drop qti-telephony-common
    * Telephony injection is not supported by 20 or newer.

--- sdm845-common-vendor.mk
+++ sdm845-common-vendor.mk
@@ -867,7 +867,6 @@ PRODUCT_PACKAGES += \
     com.qti.dpmframework \
     dpmapi \
     qcrilhook \
-    qti-telephony-common \
     android.hardware.gnss@2.1-service-qti \
     android.hardware.neuralnetworks@1.3-service-qti \
     manifest_android.hardware.drm@1.3-service.widevine \
