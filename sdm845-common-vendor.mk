29970423a42296f702277230b2ebfd5fe4986208
Han Sol Jin <hansol@hansol.ca>
2024-09-09 18:29:55 -0700

    sdm845-common: regenerate blueprints
    Change-Id: I9451baddc00e0569eafe090e38713f5de46e45ac

--- sdm845-common-vendor.mk
+++ sdm845-common-vendor.mk
@@ -779,7 +779,7 @@ PRODUCT_PACKAGES += \
     com.qti.dpmframework \
     dpmapi \
     qcrilhook \
-    android.hardware.gnss@2.1-service-qti \
-    android.hardware.neuralnetworks@1.3-service-qti \
-    manifest_android.hardware.drm@1.3-service.widevine \
-    vendor.qti.gnss@4.0-service
+    android.hardware.gnss@2.1-service-qti.xml \
+    android.hardware.neuralnetworks@1.3-service-qti.xml \
+    manifest_android.hardware.drm@1.3-service.widevine.xml \
+    vendor.qti.gnss@4.0-service.xml
